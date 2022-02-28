326b3440b6338c90c5c170d5659b925fcd1c213a
Alexei Novikov <a91n0d@gmail.com>
2022-02-28 22:04:55 +0300

    Fixed code smells.

--- TransformToWordsTask/Transformer.cs
+++ TransformToWordsTask/Transformer.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.Globalization;
 
-#pragma warning disable CA1822
-
 namespace TransformToWordsTask
 {
     /// <summary>
@@ -19,13 +17,9 @@ namespace TransformToWordsTask
 
         private readonly Dictionary<char, string> transformToWords = new Dictionary<char, string>()
             {
-                { '-', "minus " }, { '0', "zero " },
-                { '1', "one " }, { '2', "two " },
-                { '3', "three " }, { '4', "four " },
-                { '5', "five " }, { '6', "six " },
-                { '7', "seven " }, { '8', "eight " },
-                { '9', "nine " }, { 'E', "E " },
-                { '.', "point " }, { '+', "plus " },
+                { '-', "minus " }, { '0', "zero " }, { '1', "one " }, { '2', "two " },
+                { '3', "three " }, { '4', "four " }, { '5', "five " }, { '6', "six " },
+                { '7', "seven " }, { '8', "eight " }, { '9', "nine " }, { 'E', "E " }, { '.', "point " }, { '+', "plus " },
             };
 
         /// <summary>
